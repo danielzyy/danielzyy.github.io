2faf42fcf2f92485fc912c7493058d02fe6bf24e
Daniel Ye <danielye4@gmail.com>
2020-12-23 04:07:42 -0500

    Added projects section grid outline

--- js/main.js
+++ js/main.js
@@ -54,6 +54,21 @@ $(document).ready(() => {
         document.body.appendChild(css);
     };
     
+    let $btns = $('.project-area .button-group button');
+
+    $btns.click(function(e){
+        $('.project-area .button-group button').removeClass('active');
+        e.target.classList.add('active');
+
+        let selector = $(e.target).attr('data-filter');
+        $('.project-area .grid').isotope({
+            filter: selector
+        });
+        return false;
+    })
+
+    // $('.project-area .button-group #btn1').trigger('click');
+
 	// $('.gallery-item .thumbnail').on('mouseenter', event => {
 	// 	$(event.currentTarget).animate({
 	// 		opacity:1,
