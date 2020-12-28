1dfc96bf32a19de7410f16aacbbfe0bf5bd1265c
Daniel Ye <danielye4@gmail.com>
2020-12-27 21:21:06 -0500

    added footer and navbar scroll links and resume

--- js/main.js
+++ js/main.js
@@ -66,6 +66,8 @@ $(document).ready(() => {
         });
         return false;
     })
+    $('.project-area .button-group #btn1').trigger('click');
+
     //Sticky navbar
     let nav_offset_top = $('.header_area').height() + 50;
 
@@ -83,64 +85,30 @@ $(document).ready(() => {
     }
 
     navbarFixed();
-    // $('.project-area .button-group #btn1').trigger('click');
 
-	// $('.gallery-item .thumbnail').on('mouseenter', event => {
-	// 	$(event.currentTarget).animate({
-	// 		opacity:1,
-	// 		height:"22rem",
-	// 		width: "31rem"
-	// 	},290);
-	// });
-	// $('.thumbnail').on('mouseleave', event => {
-	// 	$(event.currentTarget).animate({
-	// 		opacity:0.9,
-	// 		height:"21rem",
-	// 		width: "30rem"
-	// 	},290);
-	// });
-	// $('#academics').on('click', () => {
-	// 	$('#academics').fadeOut(500);
-	// 	$('.academicsinfo').delay(500).slideDown(500); 
-	// });
-	// $('#sports').on('click', () => {
-	// 	$('#sports').fadeOut(500);
-	// 	$('.sportsinfo').delay(500).slideDown(500); 
-	// });
-	// $('#extracurriculars').on('click', () => {
-	// 	$('#extracurriculars').fadeOut(500);
-	// 	$('.extrainfo').delay(500).slideDown(500); 
-	// });
-	// $('.academicsinfo .closebutton').on('click', () => {
-	// 	$('.academicsinfo').slideUp(500); 
-	// 	$('#academics').delay(500).fadeIn(500);	
-	// });
-	// $('.sportsinfo .closebutton').on('click', () => {
-	// 	$('.sportsinfo').slideUp(500); 
-	// 	$('#sports').delay(500).fadeIn(500);	
-	// });
-	// $('.extrainfo .closebutton').on('click', () => {
-	// 	$('.extrainfo').slideUp(500); 
-	// 	$('#extracurriculars').delay(500).fadeIn(500);	
-	// });
-	// function goToByScroll(id){
-	//           // Remove "link" from the ID
-	//         id = id.replace("link", "");
-	//           // Scroll
-	//         $('html,body').animate({
-	//             scrollTop: $("#"+id).offset().top},
-	//             'slow');
-	//     }
-    // $('.bar a').click(function(e) { 
-    //       // Prevent a page reload when a link is pressed
-    //     e.preventDefault(); 
-    //       // Call the scroll function
-    //     goToByScroll($(this).attr("id"));           
-    // });
-    // $('.sidenav a').click(function(e) { 
-    //       // Prevent a page reload when a link is pressed
-    //     e.preventDefault(); 
-    //       // Call the scroll function
-    //     goToByScroll($(this).attr("id"));           
-    // });
+    // // Navbar scroll
+    function goToByScroll(id){
+        // Remove "link" from the ID
+      id = id.replace("link", "");
+        // Scroll
+      $('html,body').animate({
+          scrollTop: $("#"+id).offset().top
+           + $('#main').scrollTop() 
+          - $('#main').offset().top
+          - 55
+        },
+          'slow');
+    }
+    $('.main-menu .navbar-nav a').click(function(e) { 
+        // Prevent a page reload when a link is pressed
+    e.preventDefault(); 
+        // Call the scroll function
+    goToByScroll($(this).attr("id"));           
+    });
+    $('.footer-area a').click(function(e) { 
+        // Prevent a page reload when a link is pressed
+      e.preventDefault(); 
+        // Call the scroll function
+      goToByScroll($(this).attr("id"));           
+  });
 });
